27ebbe2b865e58b470d97292633afb6b92a53392
Ben Marlin <marlin@cs.umass.edu>
2021-07-12 22:32:50 -0400

    updates to extrasensory

--- mFlow/Blocks/data_loader_extrasensory.py
+++ mFlow/Blocks/data_loader_extrasensory.py
@@ -1,6 +1,6 @@
 import sys, os
 sys.path.insert(0, os.path.abspath('..'))
-from Workflow.compute_graph import node
+from mFlow.Workflow.compute_graph import node
 
 import requests, zipfile, io
 import glob
@@ -20,9 +20,6 @@ def __extrasensory_data_loader(label="SLEEPING",data_size="large"):
         directory = "data/extrasensory/"
     elif(data_size=="small"):
         directory = "data/small_extrasensory/"
-        df = pd.read_pickle(directory + pkl_file)
-        return({"dataframe":df}) 
-        
 
     try:
         os.stat(directory)
@@ -43,14 +40,20 @@ def __extrasensory_data_loader(label="SLEEPING",data_size="large"):
         df_list=[]
         uuid_list = []
         files = glob.glob(directory + "*.gz")
+        df_all=None
         for file in files:
+            print(file)
             uuid = os.path.basename(file).split(".")[0]
             df = pd.read_csv(file, compression='gzip', header=0, sep=',', quotechar='"')
             df_list.append(df)
             uuid_list.append(uuid)
 
+        print("Done reading files")
         df = pd.concat(df_list, axis = 0, keys=uuid_list)
+        print("Done building dataframe")
+        df_list=None
         df.to_pickle(directory + pkl_file)
+        print("Done writing pkl file")
 
     #Extract desired label
     label_str = "label:" + label
@@ -59,15 +62,6 @@ def __extrasensory_data_loader(label="SLEEPING",data_size="large"):
     df=df.rename(index=str, columns={label_str: "target"})
     df=df.drop(columns=other_labels)
     df.index.names = ["ID","Time"]
-    # df=df[:100000]
-    
-    end = time.time()
-    #print("Data Loader Time",end-start)
+
     return({"dataframe":df}) 
-    #return(0)
-
-
-    # def extrasensory_data_loader_random(**kwargs):
-    # return node(function = __extrasensory_data_loader_random, kwargs=kwargs, name="ES Data Loader")
-
-    # def __extrasensory_data_loader_random
+ 
